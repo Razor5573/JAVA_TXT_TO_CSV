cd937456de34410f949e7929cf2e2f7308232700
Никита Калугин <nikitakalugin02@gmail.com>
2022-03-29 19:48:50 +0700

    changed the comparator

--- src/SortTreeMap.java
+++ src/SortTreeMap.java
@@ -4,29 +4,21 @@ import java.lang.StringBuilder;
 public class SortTreeMap {
     public TreeMap<StringBuilder, Integer> SortByValue(TreeMap <StringBuilder, Integer> data) {
         TreeMap <StringBuilder, Integer> sortedData;
-        Comparator<StringBuilder> cmp = new Comparator<StringBuilder>() {
-            @Override
-            public int compare(StringBuilder o1, StringBuilder o2) {
-                if (data.get(o1).equals(data.get(o2)))
-                {
-                    //System.out.println("key1: " + data.get(o1) + " word1: " + o1 + " \nkey2: " + data.get(o2) + " word2: " + o2 + "\n\n");
-                    return 0;
-                }
-
-                if (data.get(o1).compareTo(data.get(o2)) > 0)
-                {
-                    //System.out.println("key1: " + data.get(o1) + " word1: " + o1 + " \nkey2: " + data.get(o2) + " word2: " + o2 + "\n\n");
-                    return -1;
-                }
-
-                else {
-                    //System.out.println("key1: " + data.get(o1) + " word1: " + o1 + " \nkey2: " + data.get(o2) + " word2: " + o2 + "\n\n");
-                    return 1;
-                }
-            }
+        Comparator<StringBuilder> cmp = (o1, o2) -> {
+            int result = data.get(o1).compareTo(data.get(o2));
+            if (result == 0)
+                return o1.compareTo(o2);
+            return -result;
         };
 
         sortedData = new TreeMap<>(cmp);
+        sortedData.putAll(data);
+        for(Map.Entry<StringBuilder, Integer> entry : sortedData.entrySet()) {
+            StringBuilder key = entry.getKey();
+            Integer value = entry.getValue();
+
+            System.out.println(key + " => " + value);
+        }
 
         return sortedData;
     }
